d85cf1c3db14785df63563fbc5f83dfbd3249613
medsonmoombe <medsonmoombe21@gmail.com>
2022-04-11 11:14:18 +0200

    Add the menu mobile list and select the menu item in the javascript file

---
+++ script.js
@@ -0,0 +1,7 @@
+// pop-up menu mobile
+const humburger = document.querySelector('.fa-bars');
+const logo =document.querySelector('.logo');
+const nav =document.querySelector('nav');
+const closeBtn = document.querySelector('.close-btn');
+const menuItems = document.querySelectorAll('.menu-item');
+const menuMobile = document.querySelector('.menu-mobile');
