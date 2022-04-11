9787ee76e62d57e68637b5eb8cd5f12837c58bc7
medsonmoombe <medsonmoombe21@gmail.com>
2022-04-11 13:58:11 +0200

    Modified script.js and style.css for linters

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
 // pop-up menu mobile
 const humburger = document.querySelector('.fa-bars');
-const logo =document.querySelector('.logo');
-const nav =document.querySelector('nav');
+const logo = document.querySelector('.logo');
+const nav = document.querySelector('nav');
 const closeBtn = document.querySelector('.close-btn');
 const menuItems = document.querySelectorAll('.menu-item');
 const menuMobile = document.querySelector('.menu-mobile');
 
-
-/*add the reset menu function*/
+/* add the reset menu function */
 
 function resetMobileMenu() {
-    menuItems.forEach((item) => item.classList.remove('show'));
-    nav.classList.remove('expand');
-    logo.classList.remove('hidden');
-    menuMobile.classList.remove('ul-list');
-    humburger.style.display = 'block';
-    closeBtn.classList.remove('.close-btn');
-  }
+  menuItems.forEach((item) => item.classList.remove('show'));
+  nav.classList.remove('expand');
+  logo.classList.remove('hidden');
+  menuMobile.classList.remove('ul-list');
+  humburger.style.display = 'block';
+  closeBtn.classList.remove('.close-btn');
+}
 
-  /* Add event listner to humbergur button*/
+/* Add event listner to humbergur button */
 
-  humburger.addEventListener('click', () => {
-    menuItems.forEach((item) => item.classList.add('show'));
-    nav.classList.add('expand');
-    logo.classList.add('hidden');
-    menuMobile.classList.add('ul-list');
-    humburger.style.display = 'none';
-    closeBtn.classList.add('close-btn');
-  
-    menuItems.forEach((item) => item.addEventListener('click', resetMobileMenu));
-  });
+humburger.addEventListener('click', () => {
+  menuItems.forEach((item) => item.classList.add('show'));
+  nav.classList.add('expand');
+  logo.classList.add('hidden');
+  menuMobile.classList.add('ul-list');
+  humburger.style.display = 'none';
+  closeBtn.classList.add('close-btn');
 
-  /* Add th event listner to the close button*/
+  menuItems.forEach((item) => item.addEventListener('click', resetMobileMenu));
+});
 
-  closeBtn.addEventListener('click', resetMobileMenu);
-  
-  function resizeWindow() {
-    if (window.innerWidth > 768) {
-        resetMobileMenu();
-    }
-  }
-  
-  window.addEventListener('resize', resetMobileMenu);
+/* Add th event listner to the close button */
+
+closeBtn.addEventListener('click', resetMobileMenu);
