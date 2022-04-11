3ce10906834af13a79b6bade1141b07dcf40992b
medsonmoombe <medsonmoombe21@gmail.com>
2022-04-11 11:17:26 +0200

    Add the resetMenu function

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const nav =document.querySelector('nav');
 const closeBtn = document.querySelector('.close-btn');
 const menuItems = document.querySelectorAll('.menu-item');
 const menuMobile = document.querySelector('.menu-mobile');
+
+
+/*add the reset menu function*/
+
+function resetMobileMenu() {
+    menuItems.forEach((item) => item.classList.remove('show'));
+    nav.classList.remove('expand');
+    logo.classList.remove('hidden');
+    menuMobile.classList.remove('ul-list');
+    humburger.style.display = 'block';
+    closeBtn.classList.remove('.close-btn');
+  }
