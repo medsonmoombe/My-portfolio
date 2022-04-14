affc0cc789fcc84c8bf9fd1b68cdf6773ae05d0a
medsonmoombe <medsonmoombe21@gmail.com>
2022-04-14 11:17:47 +0200

    Add the styles to css and modify js and also add icons file

--- script.js
+++ script.js
@@ -1,3 +1,46 @@
+const mobileMenu = document.querySelector('#menu');
+const listItem = document.querySelectorAll('.hidden-items');
+const nav = document.querySelector('nav');
+const navName = document.querySelector('#name');
+const menuList = document.querySelector('nav ul');
+const exitIcon = document.querySelector('.exit');
+const envelope = document.querySelector('#envelope');
+const projectsSection = document.querySelector('#projects');
+const blurProjects = document.querySelectorAll('#projects > div');
+
+/* Menu */
+
+function resetMobileMenu() {
+  listItem.forEach((item) => item.classList.remove('show'));
+  nav.classList.remove('expand');
+  navName.classList.remove('hidden');
+  menuList.classList.remove('ul-list');
+  mobileMenu.classList.remove('hidden');
+  exitIcon.classList.remove('X');
+  envelope.classList.remove('hidden');
+}
+
+mobileMenu.addEventListener('click', () => {
+  listItem.forEach((item) => item.classList.add('show'));
+  nav.classList.add('expand');
+  navName.classList.add('hidden');
+  menuList.classList.add('ul-list');
+  mobileMenu.classList.add('hidden');
+  exitIcon.classList.add('X');
+  envelope.classList.add('hidden');
+
+  listItem.forEach((item) => item.addEventListener('click', resetMobileMenu));
+});
+
+exitIcon.addEventListener('click', resetMobileMenu);
+
+function resizeWindow() {
+  if (window.innerWidth > 600) {
+    resetMobileMenu();
+  }
+}
+
+window.addEventListener('resize', resizeWindow);
 
 /* Project section */
 
@@ -5,7 +48,7 @@ const projects = [
   {
     id: 'main-project-button',
     title: 'Multi-Post Stories',
-    image: 'images/ImgPlaceholder.png',
+    image: 'images/Placeholder1.png',
     description:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown",
     technologies: ['css', 'html', 'bootstrap', 'Ruby'],
